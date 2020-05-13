00d88ea70c49cf30f14f73e2d16ee39f4dd95980
FR13NDS <64352865+FR13NDS666@users.noreply.github.com>
2020-05-13 21:00:27 +0700

    Update spamwa.py

--- spamwa.py
+++ spamwa.py
@@ -8,8 +8,8 @@ def tunggu(t):
 	while t:
 		wd='# Jeda selama '+str(t)+" detik "
 		print(wd,end='\r',flush=True)
-		time.sleep(5)
-		t -= 5
+		time.sleep(0)
+		t -= 0
 
 url = 'https://wapi.ruparupa.com/auth/check-otp-auth'
 url2 = 'https://wapi.ruparupa.com/auth/generate-otp'
@@ -83,8 +83,8 @@ try:
 			if z == jml:
 				break
 			else:
-				tunggu(10)
-			z += 10
+				tunggu(1)
+			z += 1
 		except requests.exceptions.ConnectionError:	
 			print("Koneksi Error!!\nPeriksa Koneksi internet Anda!!")
 		except KeyboardInterrupt:
